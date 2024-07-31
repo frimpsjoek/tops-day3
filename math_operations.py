fd4eed9a5bae11d6102c16a2a1a8ea13ddaa34af
frimpsjoek <gy1906@wayne.edu>
2024-07-31 12:11:34 -0400

    Adding a new python math operation file

---
+++ math_operations.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+
+def substract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    """This is a function that really does a multiplication of two numbers a ad b and returns the results"""
+    return a * b
+
+
+print(add(10, 5))
